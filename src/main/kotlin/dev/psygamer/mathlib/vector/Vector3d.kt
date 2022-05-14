5016537d65c5dd317a30c5b649d1d566e2783d83
psyGamer <psygamer21@gmail.com>
2022-05-14 22:21:58 +0200

    Made `Vector3d` tests pass

--- src/main/kotlin/dev/psygamer/mathlib/vector/Vector3d.kt
+++ src/main/kotlin/dev/psygamer/mathlib/vector/Vector3d.kt
@@ -18,11 +18,11 @@ open class Vector3d(val x: Double, val y: Double, val z: Double) {
 	}
 	
 	operator fun minus(other: Vector3d): Vector3d {
-		return Vector3d(this.x - other.x, this.y - other.y, this.z + other.z)
+		return Vector3d(this.x - other.x, this.y - other.y, this.z - other.z)
 	}
 	
 	operator fun times(other: Vector3d): Vector3d {
-		return Vector3d(this.x * other.x, this.y * other.y, this.z + other.z)
+		return Vector3d(this.x * other.x, this.y * other.y, this.z * other.z)
 	}
 	
 	operator fun times(scalar: Double): Vector3d {
@@ -30,15 +30,23 @@ open class Vector3d(val x: Double, val y: Double, val z: Double) {
 	}
 	
 	operator fun div(other: Vector3d): Vector3d {
+		if (other.x == 0.0 || other.y == 0.0 || other.z == 0.0)
+			throw ArithmeticException("Division by zero!")
 		return Vector3d(this.x / other.x, this.y / other.y, this.z / other.z)
 	}
 	
 	operator fun div(scalar: Double): Vector3d {
+		if (scalar == 0.0)
+			throw ArithmeticException("Division by zero!")
 		return Vector3d((this.x / scalar), (this.y / scalar), (this.z / scalar))
 	}
 	
 	infix fun cross(other: Vector3d): Vector3d {
-		return Vector3d(this.x * other.y, this.y * other.x, this.z * other.z)
+		return Vector3d(
+			this.y * other.z - this.z * other.y,
+			this.z * other.x - this.x * other.z,
+			this.x * other.y - this.y * other.x
+		)
 	}
 	
 	infix fun dot(other: Vector3d): Double {
@@ -71,4 +79,26 @@ open class Vector3d(val x: Double, val y: Double, val z: Double) {
 	
 	val inverted
 		get() = Vector3d(-this.x, -this.y, -this.z)
+	
+	override fun toString(): String {
+		return "Vector3i($x, $y, $z)"
+	}
+	
+	override fun equals(other: Any?): Boolean {
+		if (other == null)
+			return false
+		if (other !is Vector3d)
+			return false
+		if (this === other)
+			return true
+		
+		return this.x == other.x && this.y == other.y && this.z == other.z
+	}
+	
+	override fun hashCode(): Int {
+		var result = x.hashCode()
+		result = 31 * result + y.hashCode()
+		result = 31 * result + z.hashCode()
+		return result
+	}
 }
