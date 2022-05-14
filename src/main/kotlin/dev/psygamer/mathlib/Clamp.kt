d897254311b3aeaa3ee6e5cc9fb9df999c3a5ad9
psyGamer <psygamer21@gmail.com>
2022-05-15 01:35:51 +0200

    Improved Java compatibility

--- src/main/kotlin/dev/psygamer/mathlib/Clamp.kt
+++ src/main/kotlin/dev/psygamer/mathlib/Clamp.kt
@@ -1,7 +1,10 @@
+@file:JvmName("Clamp")
+
 package dev.psygamer.mathlib
 
 import kotlin.math.*
 
+@JvmOverloads
 fun Int.clamp(min: Int = 1, max: Int): Int {
 	return min(max, max(min, this))
 }
@@ -10,6 +13,7 @@ fun UInt.clamp(min: UInt = 1u, max: UInt): UInt {
 	return min(max, max(min, this))
 }
 
+@JvmOverloads
 fun Long.clamp(min: Long = 1, max: Long): Long {
 	return min(max, max(min, this))
 }
@@ -18,20 +22,24 @@ fun ULong.clamp(min: ULong = 1u, max: ULong): ULong {
 	return min(max, max(min, this))
 }
 
+@JvmOverloads
 fun Float.clamp(min: Float = 1.0f, max: Float): Float {
 	return min(max, max(min, this))
 }
 
+@JvmOverloads
 fun Double.clamp(min: Double = 1.0, max: Double): Double {
 	return min(max, max(min, this))
 }
 
+@JvmOverloads
 fun Float.clampToIntIfNear(epsilon: Float = 0.001f): Float {
 	if (abs(this - this.roundToInt()) <= epsilon)
 		return this.roundToInt().toFloat()
 	return this
 }
 
+@JvmOverloads
 fun Double.clampToIntIfNear(epsilon: Double = 0.001): Double {
 	if (abs(this - this.roundToInt()) <= epsilon)
 		return this.roundToInt().toDouble()
