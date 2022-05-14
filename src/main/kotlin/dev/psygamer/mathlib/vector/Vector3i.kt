4d7a540394abdb04f3344a16bea17b0cb31e3f83
psyGamer <psygamer21@gmail.com>
2022-05-14 22:37:47 +0200

    Made `Vector3i` tests pass

--- src/main/kotlin/dev/psygamer/mathlib/vector/Vector3i.kt
+++ src/main/kotlin/dev/psygamer/mathlib/vector/Vector3i.kt
@@ -18,35 +18,45 @@ open class Vector3i(val x: Int, val y: Int, val z: Int) {
 	}
 	
 	operator fun minus(other: Vector3i): Vector3i {
-		return Vector3i(this.x - other.x, this.y - other.y, this.z + other.z)
+		return Vector3i(this.x - other.x, this.y - other.y, this.z - other.z)
 	}
 	
 	operator fun times(other: Vector3i): Vector3i {
-		return Vector3i(this.x * other.x, this.y * other.y, this.z + other.z)
+		return Vector3i(this.x * other.x, this.y * other.y, this.z * other.z)
 	}
 	
 	operator fun times(scalar: Int): Vector3i {
 		return Vector3i((this.x * scalar), (this.y * scalar), (this.z * scalar))
 	}
 	
-	operator fun times(scalar: Float): Vector3i {
-		return Vector3i((this.x * scalar).toInt(), (this.y * scalar).toInt(), (this.z * scalar).toInt())
+	operator fun times(scalar: Float): Vector3f {
+		return Vector3f((this.x * scalar), (this.y * scalar), (this.z * scalar))
 	}
 	
 	operator fun div(other: Vector3i): Vector3i {
+		if (other.x == 0 || other.y == 0 || other.z == 0)
+			throw ArithmeticException("Division by zero!")
 		return Vector3i(this.x / other.x, this.y / other.y, this.z / other.z)
 	}
 	
 	operator fun div(scalar: Int): Vector3i {
+		if (scalar == 0)
+			throw ArithmeticException("Division by zero!")
 		return Vector3i((this.x / scalar), (this.y / scalar), (this.z / scalar))
 	}
 	
-	operator fun div(scalar: Float): Vector3i {
-		return Vector3i((this.x / scalar).toInt(), (this.y / scalar).toInt(), (this.z / scalar).toInt())
+	operator fun div(scalar: Float): Vector3f {
+		if (scalar == 0.0f)
+			throw ArithmeticException("Division by zero!")
+		return Vector3f((this.x / scalar), (this.y / scalar), (this.z / scalar))
 	}
 	
 	infix fun cross(other: Vector3i): Vector3i {
-		return Vector3i(this.x * other.y, this.y * other.x, this.z * other.z)
+		return Vector3i(
+			this.y * other.z - this.z * other.y,
+			this.z * other.x - this.x * other.z,
+			this.x * other.y - this.y * other.x
+		)
 	}
 	
 	infix fun dot(other: Vector3i): Int {
@@ -56,27 +66,49 @@ open class Vector3i(val x: Int, val y: Int, val z: Int) {
 	val magnitude: Float
 		get() = sqrt((this.x * this.x + this.y * this.y + this.z * this.z).toFloat())
 	
-	val magnitudeSquared: Float
-		get() = (this.x * this.x + this.y * this.y + this.z * this.z).toFloat()
+	val magnitudeSquared: Int
+		get() = (this.x * this.x + this.y * this.y + this.z * this.z)
 	
 	fun distance(other: Vector3i): Float {
 		return (this - other).magnitude
 	}
 	
-	fun distanceSquared(other: Vector3i): Float {
+	fun distanceSquared(other: Vector3i): Int {
 		return (this - other).magnitudeSquared
 	}
 	
-	val normalized: Vector3i
+	val normalized: Vector3f
 		get() = this / this.magnitude
 	
 	val angle: Float
 		get() = atan2(this.x.toFloat(), this.y.toFloat())
 	
 	fun angleBetween(other: Vector3i): Float {
-		return acos((this dot other / (this.magnitude * other.magnitude).toInt()).toFloat())
+		return acos((this dot other) / (this.magnitude * other.magnitude))
 	}
 	
 	val inverted
 		get() = Vector3i(-this.x, -this.y, -this.z)
+	
+	override fun toString(): String {
+		return "Vector3i($x, $y, $z)"
+	}
+	
+	override fun equals(other: Any?): Boolean {
+		if (other == null)
+			return false
+		if (other !is Vector3i)
+			return false
+		if (this === other)
+			return true
+		
+		return this.x == other.x && this.y == other.y && this.z == other.z
+	}
+	
+	override fun hashCode(): Int {
+		var result = x.hashCode()
+		result = 31 * result + y.hashCode()
+		result = 31 * result + z.hashCode()
+		return result
+	}
 }
--- src/test/kotlin/dev/psygamer/mathlib/vector/Vector3iTest.kt
+++ src/test/kotlin/dev/psygamer/mathlib/vector/Vector3iTest.kt
@@ -61,7 +61,7 @@ internal class Vector3iTest {
 	
 	@Test
 	fun getMagnitude() {
-		assertEquals(sqrt(14.0).toInt(), Vector3i(1, -2, 3).magnitude)
+		assertEquals(sqrt(14.0).toFloat(), Vector3i(1, -2, 3).magnitude, 0.001f)
 	}
 	
 	@Test
@@ -71,7 +71,7 @@ internal class Vector3iTest {
 	
 	@Test
 	fun distance() {
-		assertEquals(sqrt(24.0).toInt(), Vector3i(3, 2, 1).distance(Vector3i(1, -2, 3)))
+		assertEquals(sqrt(24.0).toFloat(), Vector3i(3, 2, 1).distance(Vector3i(1, -2, 3)), 0.001f)
 	}
 	
 	@Test
@@ -81,18 +81,18 @@ internal class Vector3iTest {
 	
 	@Test
 	fun getNormalized() {
-		assertEquals(1, Vector3i(1, 2, 4).normalized.magnitude)
+		assertEquals(1.0f, Vector3i(1, 2, 4).normalized.magnitude, 0.001f)
 	}
 	
 	@Test
 	fun getAngle() {
-		assertEquals(Math.toRadians(45.0).toInt(), Vector3i(1, 1, 0).angle)
+		assertEquals(Math.toRadians(45.0).toFloat(), Vector3i(1, 1, 0).angle, 0.001f)
 	}
 	
 	@Test
 	fun angleBetween() {
-		assertEquals(Math.toRadians(45.0).toInt(), Vector3i(1, 1, 0).angleBetween(Vector3i(1, 0, 0)))
-		assertEquals(Math.toRadians(00.0).toInt(), Vector3i(1, 0, 0).angleBetween(Vector3i(1, 0, 0)))
+		assertEquals(Math.toRadians(45.0).toFloat(), Vector3i(1, 1, 0).angleBetween(Vector3i(1, 0, 0)), 0.001f)
+		assertEquals(Math.toRadians(00.0).toFloat(), Vector3i(1, 0, 0).angleBetween(Vector3i(1, 0, 0)), 0.001f)
 	}
 	
 	@Test
